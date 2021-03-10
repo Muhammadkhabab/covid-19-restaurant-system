68ae3b62fea394424fb7d0b8e173e04d5bbe98a6
Riza Hassan <vehanwien@gmail.com>
2021-03-09 22:31:59 -0600

    Completed User schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,53 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    
+    first_name: {
+        type: String,
+        required: true,
+      },
+      last_name: {
+        type: String,
+        required: true,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      phone_number:{
+          type: Number,
+          required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      is_admin: {
+        type: Boolean,
+        default: false,
+      },
+      is_staff: {
+        type: Boolean,
+        default: false,
+      },
+      is_developer: {
+        type: Boolean,
+        default: false,
+      },
+      birth_date:{
+        type: Date,
+        required: true,
+      },
+      restaurant_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'restaurant',}
+        
+
+
 });
 
 module.exports = User = mongoose.model('user', UserSchema);
