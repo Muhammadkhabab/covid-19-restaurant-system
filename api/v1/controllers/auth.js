f737a0ab24b0fc3b8a238c5bbf4b675c26b1124f
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2021-03-13 18:47:55 -0500

    fix indentation

--- api/v1/controllers/auth.js
+++ api/v1/controllers/auth.js
@@ -13,55 +13,55 @@ dotenv.config();
 const User = require('../../../models/User');
 
 module.exports = {
-    login: async (req, res, _next) => {
-        // Check for errors.
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-    
-        // Destructuring data from request body.
-        const { email, password } = req.body;
-    
-        try {
-          // Check if user exists (check if email exists).
-        let user = await User.findOne({ email: email });
-        if (!user) {
-        return res.status(401).json({
-            errors: [{ msg: 'Invalid credentials! Please try again!' }],
-        });
-        }
+  login: async (req, res, _next) => {
+    // Check for errors.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-        // Check if password matches.
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-        return res.status(401).json({
-            errors: [{ msg: 'Invalid credentials! Please try again!' }],
-        });
-        }
+    // Destructuring data from request body.
+    const { email, password } = req.body;
 
-        // Sign and return jsonwebtoken.
-        const payload = {
+    try {
+      // Check if user exists (check if email exists).
+      let user = await User.findOne({ email: email });
+      if (!user) {
+        return res.status(401).json({
+          errors: [{ msg: 'Invalid credentials! Please try again!' }],
+        });
+      }
+
+      // Check if password matches.
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({
+          errors: [{ msg: 'Invalid credentials! Please try again!' }],
+        });
+      }
+
+      // Sign and return jsonwebtoken.
+      const payload = {
         user: {
-            id: user.id,
+          id: user.id,
         },
-        };
-        jwt.sign(
+      };
+      jwt.sign(
         payload,
         process.env.JWT_PRIVATE_KEY,
         { expiresIn: 86400 },
         (err, token) => {
-            if (err) throw err;
-            return res.status(200).json({ token });
+          if (err) throw err;
+          return res.status(200).json({ token });
         }
-        );
+      );
     } catch (err) {
-        console.error(err.message);
-        return res.status(500).json({
+      console.error(err.message);
+      return res.status(500).json({
         errors: [
-            { msg: 'Unexpected server error happened. Please try again later!' },
+          { msg: 'Unexpected server error happened. Please try again later!' },
         ],
-        });
+      });
     }
-    },
+  },
 };
