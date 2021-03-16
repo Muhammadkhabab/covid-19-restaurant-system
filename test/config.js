4e9dc647d92fec6f1880ee57637f8bab4cdd7918
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2021-03-16 08:09:17 -0400

    setup test structure

---
+++ test/config.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const models = require('./models/index');
+const routes = require('./api/v1/index');
+
+before((done) => {
+  const localDB = 'mongodb://localhost/safedining_testdb';
+  const remoteDB = process.env.MONGO_URI_DEV;
+  mongoose.connect(remoteDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  mongoose.connection
+    .once('open', () => {
+      console.log('Test database connected...');
+      mongoose.connection.db.dropDatabase(() => {
+        console.log('Test database reset...');
+        done();
+      });
+    })
+    .on('error', (err) => {
+      done(err);
+    });
+});
+
+after(() => {
+  console.log('Finish testing!');
+});
+
+describe('----- TESTING -----', () => {
+  models();
+  routes();
+});
