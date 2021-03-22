ed637dd853e026d59f937c3e4e74131a844b73a2
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2021-03-22 01:29:47 -0400

    refactor seed of restaurants

--- seeds/Restaurants.js
+++ seeds/Restaurants.js
@@ -1,61 +1,50 @@
-const faker = require("faker")
-const Restaurant = require('../models/Restaurant')
+const faker = require('faker');
+const Restaurant = require('../models/Restaurant');
 
-const generateBoolean = () =>{
-	return Math.random() < 0.5;
-}
+const generateBoolean = () => {
+  return Math.random() < 0.5;
+};
 
-const generateNumber = () =>{
-	return Math.floor((Math.random() * 100) + 1);
-}
+const generateNumber = (lower, upper) => {
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+};
 
-const generateSquareFt = () =>{
-	return Math.floor((Math.random() * 2000) + 100); 
-}
+const generateRestaurant = async () => {
+  const restaurant = new Restaurant({
+    restaurant_name: faker.company.companyName(),
+    address: faker.address.streetAddress(),
+    restaurant_email: faker.internet.email(),
+    restaurant_phone_number: faker.phone.phoneNumber(),
+    website_url: faker.internet.url(),
+    dine_in: generateBoolean(),
+    dine_outside: generateBoolean(),
+    pickup: generateBoolean(),
+    curbside_pickup: generateBoolean(),
+    delivery: generateBoolean(),
+    policy_notes: faker.lorem.sentence(),
+    employee_capacity: generateNumber(10, 25),
+    customer_capacity: generateNumber(20, 100),
+    number_tables: generateNumber(10, 20),
+    square_footage: generateNumber(300, 2000),
+    customer_per_table: generateNumber(3, 10),
+    tables_distance: generateNumber(3, 10),
+  });
 
-const generateCustPerTable = () =>{
-	return Math.floor((Math.random() * 5) + 1); 
-}
+  return new Promise(async (resolve) => {
+    await restaurant.save();
+    return resolve();
+  });
+};
 
-const generateTablesDistance= () =>{
-	return Math.floor((Math.random() * 20) + 1); 
-}
-
-const createRestaurant = async ()=>{
-	console.log("Creating Restaurants ...");
-
-	const restaurant= new Restaurant({
-		restaurant_name: faker.company.companyName(),
-		address: faker.address.streetAddress(),
-		restaurant_email: faker.internet.email(),
-		restaurant_phone_number: faker.phone.phoneNumber(),
-		website_url:faker.internet.url(),
-		dine_in: generateBoolean(),
-		dine_outside:generateBoolean(),
-		pickup:generateBoolean(),
-		curbside_pickup:generateBoolean(),
-		delivery:generateBoolean(),
-		policy_notes: faker.lorem.sentence(),
-		employee_capacity: generateNumber(),
-		customer_capacity: generateNumber(),
-		number_tables:generateNumber(),
-		square_footage: generateSquareFt(),
-		customer_per_table: generateCustPerTable(),
-		tables_distance:generateTablesDistance(),
-	});
-
-	await restaurant.save();
-	console.log("Created a restaurant");
-}
-
-
-module.exports={
-	createRestaurants: async(num)=>{
-		for(let i=0;i<num;i++){
-			await createRestaurant();
-		}
-	}
-}
-
-
-
+module.exports = {
+  generateRestaurants: async (num) => {
+    return new Promise(async (resolve) => {
+      console.log('Generating fake restaurants...');
+      for (let i = 0; i < num; i++) {
+        await generateRestaurant();
+      }
+      console.log(`${num} fake restaurants generated!`);
+      return resolve();
+    });
+  },
+};
