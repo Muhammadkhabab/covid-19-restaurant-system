b4377faee8807711ce3f9f94f799e33534ca5e33
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2021-03-26 10:50:15 -0400

    fix update account form

--- client/src/components/account/Account.js
+++ client/src/components/account/Account.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, FormGroup, Input } from 'reactstrap';
+import { Form, FormGroup, Input, Container, Row, Col } from 'reactstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
@@ -9,14 +9,14 @@ import { updateUser } from '../../actions/auth';
 const Account = ({ auth: { loading, user } }) => {
   // Set user data.
   const [data, setData] = useState({
-    firstName: '',
-    lastName: '',
+    first_name: '',
+    last_name: '',
     username: '',
     email: '',
-    phoneNumber: '',
-    oldPassword: '',
-    newPassword: '',
-    confirmedNewPassword: '',
+    phone_number: '',
+    old_password: '',
+    new_password: '',
+    confirmed_new_password: '',
   });
 
   useEffect(() => {
@@ -25,36 +25,36 @@ const Account = ({ auth: { loading, user } }) => {
 
   // Destructuring.
   const {
-    firstName,
-    lastName,
+    first_name,
+    last_name,
     username,
     email,
-    phoneNumber,
-    oldPassword,
-    newPassword,
-    confirmedNewPassword,
+    phone_number,
+    old_password,
+    new_password,
+    confirmed_newpassword,
   } = data;
 
   // Event listener for form submission.
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (newPassword.length < 6 || !/\d/.test(newPassword)) {
+    if (new_password.length < 6 || !/\d/.test(new_password)) {
       toast.error(
         'Password must be at least 6 characters and contain at least a number!'
       );
-    } else if (newPassword !== confirmedNewPassword) {
+    } else if (new_password !== confirmed_newpassword) {
       toast.error('Passwords do not match!');
     } else {
       updateUser({
-        firstName,
-        lastName,
+        first_name,
+        last_name,
         username,
         email,
-        phoneNumber,
-        oldPassword,
-        newPassword,
-        confirmedNewPassword,
+        phone_number,
+        old_password,
+        new_password,
+        confirmed_newpassword,
       });
     }
   };
@@ -65,96 +65,104 @@ const Account = ({ auth: { loading, user } }) => {
   return loading ? (
     <Spinner />
   ) : (
-    <div>
-      <Form onSubmit={onSubmit} className='authenticate-form'>
-        <h3 className='text-center text-info mb-4'>Account Update</h3>
-        <FormGroup>
-          <Input
-            type='text'
-            name='name'
-            value={firstName}
-            placeholder='Please enter your first name'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type='text'
-            name='lastName'
-            value={lastName}
-            placeholder='Please enter your last name'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type='text'
-            name='username'
-            value={username}
-            placeholder='Please enter your username'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type='email'
-            name='email'
-            value={email}
-            placeholder='Please enter your email address'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type='text'
-            name='phoneNumber'
-            value={phoneNumber}
-            placeholder='Please enter your phone number'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type='password'
-            name='oldPassword'
-            value={oldPassword}
-            placeholder='Please enter your existing password'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type='password'
-            name='newPassword'
-            value={newPassword}
-            placeholder='Please enter a new password'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type='password'
-            name='confirmedNewPassword'
-            value={confirmedNewPassword}
-            placeholder='Please confirm your new password'
-            onChange={onChange}
-            required
-          />
-        </FormGroup>
-        <Input
-          type='submit'
-          value='Update'
-          className='btn-outline-info btn-block submitFormButton'
-        />
-      </Form>
-    </div>
+    <Container className='my-4'>
+      <Row>
+        <Col
+          xs={{ size: 8, offset: 2 }}
+          md={{ size: 6, offset: 3 }}
+          lg={{ size: 4, offset: 4 }}
+        >
+          <Form onSubmit={onSubmit} className='authenticate-form'>
+            <h3 className='text-center text-info mb-4'>Account Update</h3>
+            <FormGroup>
+              <Input
+                type='text'
+                name='first_name'
+                value={first_name}
+                placeholder='Please enter your first name'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type='text'
+                name='last_name'
+                value={last_name}
+                placeholder='Please enter your last name'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type='text'
+                name='username'
+                value={username}
+                placeholder='Please enter your username'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type='email'
+                name='email'
+                value={email}
+                placeholder='Please enter your email address'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type='text'
+                name='phone_number'
+                value={phone_number}
+                placeholder='Please enter your phone number'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type='password'
+                name='old_password'
+                value={old_password}
+                placeholder='Please enter your existing password'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type='password'
+                name='new_password'
+                value={new_password}
+                placeholder='Please enter a new password'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type='password'
+                name='confirmed_newpassword'
+                value={confirmed_newpassword}
+                placeholder='Please confirm your new password'
+                onChange={onChange}
+                required
+              />
+            </FormGroup>
+            <Input
+              type='submit'
+              value='Update'
+              className='btn-outline-info btn-block submitFormButton'
+            />
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
