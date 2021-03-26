31b00b79976717705bede6c49d418b4232c6effa
Cecelia Peterson <cpeterson@nextpoint.com>
2021-03-25 19:34:51 -0500

    uncommented tests

--- test/api/v1/Restaurant/index.js
+++ test/api/v1/Restaurant/index.js
@@ -3,22 +3,22 @@ const update = require('./update');
 const updateStats = require('./updateStats');
 const deleteRestaurant = require('./deleteRestaurant');
 const getAll = require('./getAll');
-//const getMy = require('./getMy');
-//const filter = require('./filter');
-//const getChartData = require('./getChartData');
-//const getById = require('./getById');
+const getMy = require('./getMy');
+const filter = require('./filter');
+const getChartData = require('./getChartData');
+const getById = require('./getById');
 
 
 module.exports = restaurant = () => {
   describe('Restaurant routes', () => {
-    //DONEcreate();
-    //DONEupdate();
+    create();
+    update();
     updateStats();
-    //deleteRestaurant();
-    //DONEgetAll();
-    //getMy();
+    getAll();
+    getMy();
+    getById();
+    deleteRestaurant();
     //filter();
     //getChartData();
-    //getById();
   });
 };
