20f6207f9b6ec19d5fc9eed6ea13b22efba087ab
Riza Hassan <55040840+rizahassan@users.noreply.github.com>
2021-04-05 23:24:05 -0500

    Added range slider for the search filter

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import './styles/Index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
