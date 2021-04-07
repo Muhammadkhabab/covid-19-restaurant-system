ec5a0b43b834c25700f1bb593511298c161cc035
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2021-04-07 05:34:03 -0400

    loop through notifications from start to end time scheduled

--- services/notifications.js
+++ services/notifications.js
@@ -1,4 +1,5 @@
 const schedule = require('node-schedule');
+const moment = require('moment');
 const Notification = require('../models/Notification');
 
 module.exports = {
@@ -8,14 +9,21 @@ module.exports = {
       const notifs = await Notification.find();
       console.log('Start scheduling...');
       notifs.forEach((noti) => {
-        schedule.scheduleJob(noti.start_date_requested, (fireDate) => {
-          console.log(
-            'This job was supposed to run at ' +
-              fireDate +
-              ', but actually ran at ' +
-              new Date()
-          );
-        });
+        const start = moment(noti.start_time);
+        const end = moment(noti.end_time);
+        const dt = noti.interval;
+        for (let i = start; i < end; i = i.add(dt, 'm')) {
+          console.log(i.toDate());
+        }
+
+        // schedule.scheduleJob(noti.start_date_requested, (fireDate) => {
+        //   console.log(
+        //     'This job was supposed to run at ' +
+        //       fireDate +
+        //       ', but actually ran at ' +
+        //       new Date()
+        //   );
+        // });
       });
     } catch (err) {
       console.error(err.message);
