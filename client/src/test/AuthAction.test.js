02d7f5477fa522682e5aef1bbcfe9c55eb853349
wboettge <wboettge@wisc.edu>
2021-04-30 13:06:46 -0500

    src/test/AuthAction.test.js

--- client/src/test/AuthAction.test.js
+++ client/src/test/AuthAction.test.js
@@ -11,20 +11,58 @@ const API = 'api/v1';
 
 jest.mock('axios');
 
+const mockedData = {
+  data: {
+    msg: 'test',
+  }
+}
+
 describe('auth async actions', () => {
 
   it('loads user', () => {
-    const mockedData = {
-      data: {
-        msg: 'User not found!',
-      }
-    }
+
     axios.get.mockImplementationOnce(() => Promise.resolve(mockedData));
 
-    const expectedAction = [{"payload": {"msg": "User not found!"}, "type": "USER_LOADED"}];
+    const expectedAction = [{"payload": {"msg": "test"}, "type": "USER_LOADED"}];
     const store = mockStore({});
 
     return store.dispatch(actions.loadUser()).then(() => {
+      expect(store.getActions()).to.deep.equal(expectedAction);
+    });
+  });
+
+  it('fails to load a user', () => {
+    const error = {
+        response: {
+          data: {
+            errors: [
+              {param: "test", msg: "test"}
+            ]
+          }
+        }
+    }
+    axios.get.mockImplementationOnce(() => Promise.reject(error));
+
+    const expected = [{"type": "AUTH_ERROR"}];
+
+    const store = mockStore({});
+    return store.dispatch(actions.loadUser()).then(() => {
+      expect(store.getActions()).to.deep.equal(expected)
+    })
+  })
+
+  it('registers a user', () => {
+
+    axios.post.mockImplementationOnce(() => Promise.resolve(mockedData));
+    axios.get.mockImplementationOnce(() => Promise.resolve(mockedData));
+
+    const expectedAction = [
+      {"payload": undefined, "type": "REGISTER_SUCCESS"},
+      {"payload": {"msg": "test"}, "type": "USER_LOADED"},
+    ];
+    const store = mockStore({});
+
+    return store.dispatch(actions.register({usr: "test"})).then(() => {
       expect(store.getActions()).to.deep.equal(expectedAction);
     });
   });
